1d62196934ab8543347cfb3bac626557b5f6e7b7
mikerasch <raschmichael10@gmail.com>
2022-08-11 03:21:03 -0500

    Added commandLineRunner test

--- src/main/java/com/michael/mediareview/MediaReviewApplication.java
+++ src/main/java/com/michael/mediareview/MediaReviewApplication.java
@@ -1,12 +1,42 @@
 package com.michael.mediareview;
 
+import com.michael.mediareview.media.Media;
+import com.michael.mediareview.media.MediaRepository;
+import com.michael.mediareview.media.auth.ApplicationUser;
+import com.michael.mediareview.media.auth.ApplicationUserDao;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+
+import static com.michael.mediareview.security.ApplicationUserRoles.ADMIN;
 
 @SpringBootApplication
 public class MediaReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediaReviewApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(PasswordEncoder passwordEncoder, ApplicationUserDao applicationUserDao){
+		return args -> {
+			ApplicationUser user = new ApplicationUser(
+					ADMIN.getGrantedAuthorities(),
+					"michael",
+					passwordEncoder.encode("password"),
+					true,
+					true,
+					true,
+					true
+			);
+			user.addMedia(new Media("test","test.com",6,"very good"));
+			applicationUserDao.save(user);
+
+		};
 	}
 }
